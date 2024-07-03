981049035bf682187774b362455692b395eb5ec0
Linas819 <63401219+Linas819@users.noreply.github.com>
2024-07-03 09:51:24 +0300

    Init commit

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 import './custom.css';
 
 export default class App extends Component {
