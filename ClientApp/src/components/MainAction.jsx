92fa6f101a2497fa2e062f0e86b012e2a33a017f
Linas819 <63401219+Linas819@users.noreply.github.com>
2024-07-21 07:49:44 +0300

    Login authentication user access check

--- ClientApp/src/components/MainAction.jsx
+++ ClientApp/src/components/MainAction.jsx
@@ -1,6 +1,21 @@
 import axios from "axios";
 import { SET_ERROR_MESSAGE, SET_ERROR_MODAL, SET_LOGIN, SET_USER_ID, SET_USER_ACCESS } from "./MainReducer";
 
+export const accessRights = [
+    {
+        accessId: "inv001",
+        pathname: "/warehouseinventory"
+    },
+    {
+        accessId: "ord002",
+        pathname: "/orders"
+    },
+    {
+        accessId: "reg003",
+        pathname: "/register"
+    },
+]
+
 export const LoginUser = (user, history) => {
     return async (dispatch) => {
         let result = await axios.post(`user`, user);
@@ -19,19 +34,41 @@ export const LoginUser = (user, history) => {
 
 export const LoginAuthentication = (history) => {
     return async (dispatch, getstate) => {
-        const { isLoggedIn } = getstate().main; 
+        const { pathname } = history.location;
+        const { isLoggedIn, userAccess } = getstate().main; 
         let token = sessionStorage.getItem('token');
-        if(!isLoggedIn && token)
+        if(isLoggedIn && userAccess)
         {
+            if(pathname === "/menu")
+            {
+                return;
+            }
+            else
+            {
+                const accessRightIndex = accessRights.findIndex(element => element.pathname === pathname);
+                const userAccessIndex = userAccess.findIndex(element => element === accessRights[accessRightIndex].accessId);
+                if(userAccessIndex > -1)
+                {
+                    return;
+                } else {
+                    history.push("/menu");
+                }
+            }
+        } else if(!isLoggedIn && token) {
             dispatch(SetAuthorizationToken(token));
             let result = await axios.get(`user`);
-            dispatch(SetAuthorizationToken(result.data.token));
+            const { data } = result;
+            dispatch(SetAuthorizationToken(data.token));
             dispatch({type: SET_LOGIN, value: true});
-            dispatch({type: SET_USER_ID, value: result.data.userId});
-            dispatch({type: SET_USER_ACCESS, value: result.data.userAccess});
-            history.push(history.location.pathname);
-        }
-        else
+            dispatch({type: SET_USER_ID, value: data.userId});
+            dispatch({type: SET_USER_ACCESS, value: data.userAccess});
+            const accessRightIndex = accessRights.findIndex(element => element.pathname === pathname);
+            const userAccessIndex = data.userAccess.findIndex(element => element === accessRights[accessRightIndex].accessId);
+            if(userAccessIndex > -1)
+                return;
+            else
+                history.push("/menu")
+        } else 
             history.push("/");
     }
 }
--- ClientApp/src/components/Menu.js
+++ ClientApp/src/components/Menu.js
@@ -6,18 +6,19 @@ import { connect } from 'react-redux';
 import { LoginAuthentication } from './MainAction';
 import ErrorModal from './ErrorModal';
 import { SetErrorModal } from './MainAction';
+import { accessRights } from './MainAction';
 
 class Menu extends Component {
     componentDidMount = () => {
         this.props.LoginAuthentication(this.props.history);
     }
     onClickHandler = (event, data) => {
-        const names = data.name.split("|");
-        const pathname = "/"+names[1];
-        const accessId = names[0];
+        const buttonIndex = parseFloat(data.name);
         const { userAccess } = this.props.main;
-        if(userAccess.includes(accessId))
-            this.props.history.push(pathname);
+        const pageAccessRight = accessRights[buttonIndex];
+        const userAccessRightIndex = userAccess.findIndex(element => element === pageAccessRight.accessId);
+        if(userAccessRightIndex>-1)
+            this.props.history.push(pageAccessRight.pathname);
         else
             this.props.SetErrorModal(true, "User does not have access for this page");
     }
@@ -26,9 +27,9 @@ class Menu extends Component {
             <div className='container'>
                 <AppHeader/>
                 <div className='containerItems'>
-                    <Button color='green' style={{marginTop: '20px'}} name="inv001|warehouseinventory" onClick={this.onClickHandler}>Inventory</Button>
-                    <Button color='blue' style={{marginTop: '20px'}} name="ord002|orders" onClick={this.onClickHandler}>Orders</Button>
-                    <Button color='yellow' style={{marginTop: '20px'}} name="reg003|register" onClick={this.onClickHandler}>Register</Button>
+                    <Button color='green' style={{marginTop: '20px'}} name="0" onClick={this.onClickHandler}>Inventory</Button>
+                    <Button color='blue' style={{marginTop: '20px'}} name="1" onClick={this.onClickHandler}>Orders</Button>
+                    <Button color='yellow' style={{marginTop: '20px'}} name="2" onClick={this.onClickHandler}>Register</Button>
                 </div>
                 <ErrorModal/>
             </div>
